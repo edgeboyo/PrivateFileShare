b2fa61631a7dbde29a9223436986aedc8c8e1073
edgeboyo <mkolybko@gmail.com>
2022-09-19 21:18:17 +0200

    Basic package and directory setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const { setUpFastify } = require("./src/routings");
+
+console.log("Starting Application...");
+
+const fastify = setUpFastify();
+
+console.log("Starting listener...");
+
+fastify.listen(
+  (process.env.PORT,
+  "0.0.0.0",
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  })
+);
---
+++ src/routings/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+function createFastify() {
+  const fastify = require("fastify")({
+    logger: false,
+  });
+
+  // fastify.register(require("@fastify/static"), {
+  //   root: path.join(__dirname, "public"),
+  //   prefix: "/", // optional: default '/'
+  // });
+
+  return fastify;
+}
+
+function createRoutings(fastify) {
+  // Root directory - upload site
+  fastify.get("/", async (request, reply) => {
+    reply.view("Hello!");
+  });
+}
+
+function setUpFastify() {
+  const fastify = createFastify();
+
+  createRoutings(fastify);
+
+  return fastify;
+}
+
+module.exports = { setUpFastify };
